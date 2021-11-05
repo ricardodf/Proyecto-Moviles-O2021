1fbbb0dc25070326784e7beb07b5955239fd5091
Ricardo Diaz <39528804+ricardodf@users.noreply.github.com>
2021-11-05 02:22:23 -0600

    Home: Temp user test

--- lib/screens/Home/home.screen.dart
+++ lib/screens/Home/home.screen.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:planificador_escolar/data/user.model.dart';
+import 'package:planificador_escolar/screens/Login/login.screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,6 +12,22 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  User? user = FirebaseAuth.instance.currentUser;
+  UserModel currentUser = UserModel();
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(user!.uid)
+        .get()
+        .then((value) {
+      this.currentUser = UserModel.fromMap(value.data());
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,11 +47,21 @@ class _HomeScreenState extends State<HomeScreen> {
                         child: Image.asset("assets/images/main_logo.png",
                             fit: BoxFit.contain)),
                     SizedBox(height: 10),
-                    Text("Esta la pagina de Home",
+                    Text("Bienvenido ${currentUser.name}",
                         style: TextStyle(fontSize: 20)),
                     SizedBox(height: 20),
-                    ActionChip(label: Text("Cerrar sesión"), onPressed: () {})
+                    ActionChip(
+                        label: Text("Cerrar sesión"),
+                        onPressed: () {
+                          logout(context);
+                        })
                   ],
                 ))));
+  }
+
+  Future<void> logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => LoginScreen()));
   }
 }
